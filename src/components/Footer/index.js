5f4ecb40fba59ddfc11e42911729b90b0354cd4c
Maximoo <maximtz99@gmail.com>
2022-12-01 03:33:10 -0600

    added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Footer () {
+    return (
+        <footer>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"></link>
+            <ul className='flex-row'>
+                <li>
+                    <a href='https://github.com/maximtz13'><i class="fa fa-github fa-4x"></i></a>
+                </li>
+                <li>
+                    <a href='https://www.linkedin.com/in/chaz-graham-a95a43258'><i class="fa fa-linkedin-square fa-4x"></i></a>
+                </li>
+                <li>
+                    <a href='https://stackoverflow.com/users/19429367/chaz-graham'><i class="fa fa-stack-overflow fa-4x"></i></a>
+                </li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
