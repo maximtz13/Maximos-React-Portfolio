4fe33145a1e41d39ae4d46cb8d4a845929003f18
Maximoo <maximtz99@gmail.com>
2022-12-01 03:29:52 -0600

    added index.js component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import coverImage from '../../assets/cover/cover-image.jpg';
+import profImg from '../../assets/profile/profImg.jpg';
+
 function About() {
+
   return (
-    <section className="my-5">
-      <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-      <div className="my-2">
+    <section className="" >
+      <h2 id="about">About Me</h2>
+      <img className='profile-img' src={profileImg} alt="chaz Graham" />
+      <div className="">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
-          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
-          mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum.
-          Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique
-          justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea
-          dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh.
-          Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+          Information Technology Technician studying to earn a Full-Stack Web Developer certificate. With experience ranging from simply digging holes to setting up powerline structures. His background includes lineman/groundsman, general labor, side-jobs such as construction or selling lumber, and a variety of maintenance and repair jobs. Although Maximo's past jobs are far from tech he has a passion and drive to jumpstart his tech-career! Whenever he is not doing labor-intensive work or working his I.T. fingers, he is home studying web-development in hopes of changing his career path.
         </p>
       </div>
     </section>
